e0f48cf7688e869503887591ad46a1b38204ed35
Graeme Britz <grbritz@gmail.com>
2015-05-26 16:08:33 -0700

    some initial streamgraph code

--- public/js/stream.js
+++ public/js/stream.js
@@ -2,8 +2,40 @@ var d3 = require('d3');
 
 
 
-var stream = function() {
 
+var StreamGraph = function() {
+  var self = this,
+      timeGrouping = 'millisecond', // TODO: variable
+      collectionName = 'lakemba', // TODO: variable 
+      parentDiv = '#stream',
+      margin = { top: 0, right: 70, bottom: 20, left: 90 },
+      width = 860 - margin.left - margin.right,
+      height = 150 - margin.top - margin.bottom;
+
+  function dataPath() {
+    return '/public/data/' + collectionName + '/' + timeGrouping + '-volume.json';
+  }
+
+
+  function drawChart(dataset) {
+    var x = d3.time.scale()
+              .range([0, width])
+
+
+
+  }
+
+
+  function init(collectionName, timeGrouping) {
+    // Initialize by loading the data
+    d3.json(dataPath(), function(err, data) {
+      if (err) return console.warn(err);
+      
+      dataset = data;    
+      drawChart(dataset);
+    });  
+  }
+
+  init(collectionName, timeGrouping);
 };
-
-module.exports = stream;
+module.exports = StreamGraph;
