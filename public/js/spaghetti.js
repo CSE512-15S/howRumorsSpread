f9324469cccaed2bd5da8702928b41616a50d6d5
Zeno Koller <zeno.koller@gmail.com>
2015-05-27 11:41:13 -0700

    Rewritten most of spaghetti.js such that spaghetti and voronoi are components for easy and efficient updating / rescaling

--- public/js/spaghetti.js
+++ public/js/spaghetti.js
@@ -1,24 +1,156 @@
 var d3 = require('d3');
 
 var data;
-var svg, xScale, yScale, yAxis, linecolor, voronoiGroup;
-var clock, clockText;
+var svg, spaghetti, voronoi;
 var mainViewModel;
-var isLinearScale = true;
 var margin = { top: 20, right: 70, bottom: 60, left: 90 },
 			    width = 860 - margin.left - margin.right,
 			    height = 520 - margin.top - margin.bottom;
 
+var Spaghetti = function() {
+
+	var isLinearScale = true;
+
+	var xScale = d3.time.scale(), 
+		yScale = { linear: d3.scale.linear(),
+				   log: d3.scale.log() };
+
+	var linecolor = d3.scale.ordinal()
+		.domain(["Affirm", "Deny", "Unrelated"])
+		.range(["#2c7fb8", "#c51b8a", "#bdbdbd"]);
+
+	var line = {};
+	line.linear = d3.svg.line()
+		.x(function(d) { return xScale(d.timestamp); })
+		.y(function(d) { return yScale.linear(d.popularity); })
+		.interpolate("basis");
+	line.log = d3.svg.line()
+		.x(function(d) { return xScale(d.timestamp); })
+		.y(function(d) { return yScale.log(d.popularity); })		
+		.interpolate("basis");
+
+	// Sets up the chart, precomputes paths
+	var spaghetti = function(selection) {
+		selection.each(function(data) { 
+			// Data join
+			var tweets = d3.select(this).selectAll("path")
+				.data(data, function(d) { return d.id; });
+
+			// Enter: Create paths
+			var enter = tweets.enter().append("path")
+				.attr("stroke-width", 2)
+				.attr("fill", "none")
+		      	.attr("stroke", function(d) {  return linecolor(d.first_code); });
+			
+			// Bind line and precomputed paths to the data for fast lin / log update
+			enter.each(function(d) {
+				d.line = this;
+				d.paths = {};
+				d.paths.linear = line.linear(d.points);
+				d.paths.log = line.log(d.points);
+			});
+		});
+
+		selection.call(spaghetti.draw);
+	};
+
+	// Does the actual path drawing. Useful to change the scale
+	spaghetti.draw = function(selection) {
+		selection.each(function(data) { 
+			d3.select(this).selectAll("path")
+				.transition().duration(1000)
+				.attr("d", function(d) { return isLinearScale ? d.paths.linear : d.paths.log });
+		});
+	}
+
+	// Setter/Getter methods
+	spaghetti.isLinearScale = function(value) {
+		if (!arguments.length) {
+			return isLinearScale;
+		}
+		isLinearScale = value;
+		return spaghetti;
+	};
+
+	spaghetti.xScale = function(value) {
+        if (!arguments.length) {
+            return xScale;
+        }
+        xScale = value;
+        return spaghetti;
+    };
+
+   	spaghetti.yScale = function(value) {
+   		// Only accept inputs of the form { lin: ..., log: ... }
+        if (!arguments.length || !(_.has(value, 'linear') && _.has(value, 'log'))) {
+            return yScale;
+        }
+        yScale.linear = value.linear; yScale.log = value.log;
+        return spaghetti;
+    };
+
+	return spaghetti;
+}
+
+var Voronoi = function() {
+
+	var xScale = d3.time.scale(), 
+		yScale = d3.scale.linear();
+
+	var voronoiFunction = d3.geom.voronoi()
+		.x(function(d) { return xScale(d.timestamp); })
+		.y(function(d) { return yScale(d.popularity); })
+		.clipExtent([[-margin.left, -margin.top], [width + margin.right, height + margin.bottom]]);
+
+	var voronoiData;
+
+	var voronoi = function(selection) {
+		selection.each(function(data) { 
+			// Do data setup
+			voronoiData = voronoiFunction(d3.nest()
+		        .key(function(d) { return xScale(d.timestamp) + "," + yScale(d.popularity); })
+		        .rollup(function(v) { return v[0]; })
+		        .entries(d3.merge(data.map(function(d) { return d.points; })))
+		        .map(function(d) { return d.values; }));
+		});
+		selection.call(voronoi.draw);
+	}	
+	
+	// Does the actual drawing
+	voronoi.draw = function(selection) {
+		selection.each(function(data) { 
+			d3.select(this).selectAll("path")
+					.data(voronoiData)
+				  .enter().append("path")
+					.attr("d", function(d) { return "M" + d.join("L") + "Z"; })
+		      		.datum(function(d) { return d.point; })
+		      		.on("mouseover", mouseoverVoronoi)
+			    	.on("mouseout", mouseoutVoronoi);
+		});
+	}
+
+	// Setter/Getter methods
+	voronoi.xScale = function(value) {
+        if (!arguments.length) {
+            return xScale;
+        }
+        xScale = value;
+        return voronoi;
+    };
+
+    voronoi.yScale = function(value) {
+        if (!arguments.length) {
+            return yScale;
+        }
+        yScale = value;
+        return voronoi;
+    };
+
+	return voronoi;
+}
+
 var init = function(model) {
 	mainViewModel = model;
-
-	// Event listeners for lin / log scale buttons
-	d3.select('#scale-linear').on("click", function() {
-		updateScale(true);
-	});
-	d3.select('#scale-log').on("click", function() {
-		updateScale(false);
-	});
 
 	// One-time setup
 	svg = d3.select("#spaghetti .svgContainer")
@@ -28,10 +160,6 @@ var init = function(model) {
 	  .append("g")
 		.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-	linecolor = d3.scale.ordinal()
-		.domain(["Affirm", "Deny", "Unrelated"])
-		.range(["#2c7fb8", "#c51b8a", "#bdbdbd"]);
-
 	// General setup
 	svg.append("g")
 		.attr("class", "y axis")
@@ -40,27 +168,7 @@ var init = function(model) {
 	svg.append("g")
 		.attr("class", "tweets");
 
-	clock = svg.append("g")
-		.attr("class", "clock")
-		.attr("transform", "translate(0," + 460 + ")");
-	
-	clock.append("rect")
-	  	.attr("width", 70)
-	  	.attr("height", 25)
-	  	.attr("transform", "translate(-35, 0)");
-
-	clock.append("line")
-		.attr("x1", 0).attr("y1", 0)
-		.attr("x2", 0).attr("y2", -460);
-
-	clockText = clock.append("text")
-		.attr("font-size", "14px")
-		.attr("font-weight", "bold")
-		.attr("transform", "translate(-28,18)")
-		.text("Time");
-
-	svg.on("mousemove", mousemove);
-
+	svg.on("mousemove", mousemoveSVG);
 
 	// Load data
 	d3.json('data/spaghetti/grouped.json', function(error, json) {
@@ -79,147 +187,107 @@ var init = function(model) {
 		});
 
 		// get data bounds
-		xBounds = d3.extent(d3.merge([data.map(function(d) {
+		var xBounds = d3.extent(d3.merge([data.map(function(d) {
 			return d.points[0].timestamp;
 		}), data.map(function(d) {
 			return d.points[d.points.length - 1].timestamp;
 		})]));
 
-		yBounds = d3.extent(d3.merge([data.map(function(d) {
+		var yBounds = d3.extent(d3.merge([data.map(function(d) {
 			return d.points[0].popularity;
 		}), data.map(function(d) {
 			return d.points[d.points.length - 1].popularity;
 		})]));
 
 		// scale & axis setup
-		xScale = d3.scale.linear()
+		var xScale = d3.scale.linear()
 			.domain(xBounds)
 			.range([0,width]);
-		yScale = { linear: d3.scale.linear()
+		var yScale = {};
+		yScale.linear = d3.scale.linear()
 				.domain(yBounds)
-				.range([height, 0]),
-			  log: d3.scale.log().clamp(true)
+				.range([height, 0]);
+		yScale.log = d3.scale.log().clamp(true)
 				.domain([Math.max(1, yBounds[0]), yBounds[1]])
 				.range([height, 0])
-				.nice()
-		};
+				.nice();
 
-		yAxis = d3.svg.axis()
+		var yAxis = d3.svg.axis()
 			.scale(yScale.linear)
 			.orient("left");
 
-		// Data join
-		var tweets = d3.select(".tweets").selectAll("path")
-			.data(data, function(d) { return d.id; });
+		// Create spaghetti chart and bind x and y scales
+		spaghetti = Spaghetti()
+			.xScale(xScale)
+			.yScale(yScale);
 
-		// Enter: Create paths
-		var enter = tweets.enter().append("path")
-			.attr("stroke-width", 2)
-			.attr("fill", "none") 
-	      	.attr("stroke", function(d) {  return linecolor(d.first_code); });
+		// Bind data to a selection and call the chart
+		d3.select('.tweets')
+			.datum(data)
+			.call(spaghetti);
 
-		// Precompute paths
-		var lineLinear = d3.svg.line()
-			.x(function(d) { return xScale(d.timestamp); })
-			.y(function(d) { return yScale.linear(d.popularity); })
-			.interpolate("basis");
+		// Event listeners for lin / log scale buttons
+		d3.select('#scale-linear').on("click", function() { changeScale(true) });
+		d3.select('#scale-log').on("click", function() { changeScale(false) });
 
-		var lineLog = d3.svg.line()
-			.x(function(d) { return xScale(d.timestamp); })
-			.y(function(d) { return yScale.log(d.popularity); })
-			.interpolate("basis");
+		// Add voronoi tesselations
+		var voronoi = {};
+		voronoi.linear = Voronoi()
+			.xScale(xScale)
+			.yScale(yScale.linear);
+		voronoi.log = Voronoi()
+			.xScale(xScale)
+			.yScale(yScale.log);
 
-		// bind line and path strings to the data
-		enter.each(function(d) {
-			d.line = this;
-			d.paths = {
-				linear: lineLinear(d.points),
-				log: lineLog(d.points)
-			};
-		});
+		voronoiGroup = {};
+		voronoiGroup.linear = svg.append("g").attr("class", "voronoi linear");
+		voronoiGroup.log = svg.append("g").attr("class", "voronoi log");
 
-		// Precompute voronoi tesselations
-		var voronoi = {
-			linear: d3.geom.voronoi()
-				.x(function(d) { return xScale(d.timestamp); })
-				.y(function(d) { return yScale.linear(d.popularity); })
-				.clipExtent([[-margin.left, -margin.top], [width + margin.right, height + margin.bottom]]),
-			log: d3.geom.voronoi()
-				.x(function(d) { return xScale(d.timestamp); })
-				.y(function(d) { return yScale.log(d.popularity); })
-				.clipExtent([[-margin.left, -margin.top], [width + margin.right, height + margin.bottom]])
-		};
+		// Bind data to voronoi selections and call to setup
+		voronoiGroup.linear
+			.datum(data)
+			.call(voronoi.linear);
+		voronoiGroup.log
+			.datum(data)
+			.call(voronoi.log);
 
-		voronoiGroup = {
-			linear: svg.append("g").attr("class", "voronoi linear"),
-			log: svg.append("g").attr("class", "voronoi log")
-		};
-
-		var voronoiData = {
-			linear: voronoi.linear(d3.nest()
-		        .key(function(d) { return xScale(d.timestamp) + "," + yScale.linear(d.popularity); })
-		        .rollup(function(v) { return v[0]; })
-		        .entries(d3.merge(data.map(function(d) { return d.points; })))
-		        .map(function(d) { return d.values; })),
-			log: voronoi.log(d3.nest()
-		        .key(function(d) { return xScale(d.timestamp) + "," + yScale.log(d.popularity); })
-		        .rollup(function(v) { return v[0]; })
-		        .entries(d3.merge(data.map(function(d) { return d.points; })))
-		        .map(function(d) { return d.values; }))
-		};
-
-		voronoiGroup.linear.selectAll("path")
-			.data(voronoiData.linear)
-		  .enter().append("path")
-			.attr("d", function(d) { return "M" + d.join("L") + "Z"; })
-      		.datum(function(d) { return d.point; })
-      		.on("mouseover", mouseover)
-	    	.on("mouseout", mouseout);	
-
-		voronoiGroup.log.selectAll("path")
-			.data(voronoiData.log)
-		  .enter().append("path")
-			.attr("d", function(d) { return "M" + d.join("L") + "Z"; })
-      		.datum(function(d) { return d.point; })
-      		.on("mouseover", mouseover)
-	    	.on("mouseout", mouseout);	
-
-	    // Draw paths
-		updateScale(isLinearScale);
+		// Set pointer events
+		voronoiGroup.linear.selectAll("path").attr("pointer-events", "all");
+		voronoiGroup.log.selectAll("path").attr("pointer-events", "none");
 	});
 };
 
-// Draws paths, given lin or log scale
-var updateScale = function(showLinearScale) {
-	isLinearScale = showLinearScale;
+var changeScale = function(isLinearScale) {
+	spaghetti.isLinearScale(isLinearScale);
+	d3.select('.tweets').call(spaghetti.draw);
 
-	// Update axis
-	yAxis.scale(isLinearScale ? yScale.linear : yScale.log);
-	d3.select('.y.axis').transition().duration(1000).call(yAxis);
-
-	// Switch voronoi handlers between tesselations
-	var offGroup = isLinearScale ? voronoiGroup.log : voronoiGroup.linear;
-	var onGroup = isLinearScale ? voronoiGroup.linear : voronoiGroup.log;
+	// Switch voronoi tesselations
+	voronoiLinear = d3.select('.voronoi.linear');
+	voronoiLog = d3.select('.voronoi.log');
+	var offGroup = isLinearScale ? voronoiLog : voronoiLinear;
+	var onGroup = isLinearScale ? voronoiLinear : voronoiLog;
 
 	offGroup.selectAll("path").attr("pointer-events", "none");
-	onGroup.selectAll("path").attr("pointer-events", "all");
-	
-	// Data join
-	var tweets = d3.select(".tweets").selectAll("path")
-		.data(data, function(d) { return d.id; });
+	onGroup.selectAll("path").attr("pointer-events", "all");	
 
-    // Update: Update path
-    tweets.transition().duration(1000).attr("d", function(d) { return isLinearScale ? d.paths.linear : d.paths.log; });
+	/*
+	// TO DO: update y Axis
+	yAxis.scale(isLinearScale ? yScale.linear : yScale.log);
+	d3.select('.y.axis').transition().duration(1000).call(yAxis);
+	*/
 }
 
-var mousemove = function(d) { // attatch to svg
+var mousemoveSVG = function(d) { 
 	// move clock, change time
 	var x = d3.mouse(this)[0];
-	clock.attr("transform", "translate(" + x + ",460)");
-	clockText.text(new Date(xScale.invert(x)).toTimeString().replace(/.*(\d{2}:\d{2}:\d{2}).*/, "$1"));
+	// var timestamp = spaghetti.xScale.invert(x);
+	// var timeString = new Date(timestamp).toTimeString().replace(/.*(\d{2}:\d{2}:\d{2}).*/, "$1");
+
+	// TO DO: move scanline
+	// TO DO: Call global time update here
 }
 
-var mouseover = function(d) { // attach to voronoi
+var mouseoverVoronoi = function(d) {
 	// Highlight line
 	d3.select(d.tweet.line).classed("tweet-hover", true);
 	d.tweet.line.parentNode.appendChild(d.tweet.line);
@@ -227,11 +295,11 @@ var mouseover = function(d) { // attach to voronoi
 	// TO DO
 }
 
-var mouseout = function(d) { // attach to svg
-	// Unhighlight line
+var mouseoutVoronoi = function(d) { 
+	// Unhighlight path
 	d3.select(d.tweet.line).classed("tweet-hover", false);
-	// Hide clock
-	clock.attr("transform", "translate(-200,0)");
+	// Hide scanline
+	// scanline.attr("transform", "translate(-200,0)");
 }
 
 exports.init = init;
